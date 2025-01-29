c4fb2b18227d6db4355d209217abba91766381ea
appindus-zsmith <zanesmith@faceplacephoto.com>
2025-01-29 17:50:37 -0500

    Init

---
+++ src/test/java/org/example/hubspot/HubspotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hubspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
